1d0e76cc917866bb869e46fe54641b3e3e0c2365
STUST <STUST@C306-19>
2024-11-12 10:52:46 +0800

    加入專案檔案。

---
+++ no1/no1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int month = 1;
+	string result = "";
+	cout << "請輸入月份(1-12): ";
+	cin >> month;
+
+	if (month < 1 || month > 12) {
+		cout << "請輸入有效月份" << endl;
+	}
+	else {
+		if (month >= 3 && month <= 5) result = "春季";
+		else if (month >= 6 && month <= 8) result = "夏季";
+		else if (month >= 9 && month <= 11) result = "秋季";
+		else result = "冬季";
+		cout << month << "月是" << result << endl;
+	}
+}
